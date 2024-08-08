c751ced48ddd0795a59056a5474183c883372db2
Jing Yu <ajing.yu@mail.utoronto.ca>
2024-08-08 17:23:05 -0400

    Revert "remove Firebase configuration"
    This reverts commit 79b28dc838f0904bf017705e58b5cbfab4867b18.

--- firebase.js
+++ firebase.js
@@ -6,7 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-
+const firebaseConfig = {
+  apiKey: "AIzaSyCODSeS4Hf4OYldZ9I9hxP5A97Hu4aDUaY",
+  authDomain: "inventory-management-d5852.firebaseapp.com",
+  projectId: "inventory-management-d5852",
+  storageBucket: "inventory-management-d5852.appspot.com",
+  messagingSenderId: "68903210520",
+  appId: "1:68903210520:web:34a3ad5936101d8a24fd6d",
+  measurementId: "G-RLV2FG41CM"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
